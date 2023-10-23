d75ef96d9a55aa1c97a23b7404d33661c10db15a
Makenki <ishorunmu@gmail.com>
2023-10-23 15:39:41 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const PORT = 5000;
+const goalRouter = require('./routes/goalRouter')
+
+//midlle ware
+app.use(express.json())
+
+//routes
+app.use('/api/goals', goalRouter)
+//db connection
+const startServer = async () =>{
+    try {
+        await mongoose.connect(process.env.MONGO_URI,{
+            dbName: "Goals",
+        });
+
+        app.listen(PORT, () => {
+            console.log(`server running on port : ${PORT}..`);
+        });
+        } catch (error) {
+            console.log(error);
+    }
+};
+startServer()
+
+//error route
+app.use((req,res) =>{
+res.status(404).json({success: false, msg: "resource not found"})
+})
